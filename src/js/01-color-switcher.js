cbdce43fc973ad2f1ead4fc3c4ad2c37a652d224
Maksym Kikot <maxym1111111@gmail.com>
2023-02-03 04:56:59 +0200

    first task has done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  startBtnEl: document.querySelector('[data-start]'),
+  stopBtnEl: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let colorIntervalId = null;
+
+refs.startBtnEl.addEventListener('click', onClickStartBtn);
+refs.stopBtnEl.addEventListener('click', onClickStopBtn);
+
+function onClickStartBtn() {
+  colorIntervalId = setInterval(generateColor, 1000);
+
+  refs.stopBtnEl.removeAttribute('disabled');
+  refs.startBtnEl.setAttribute('disabled', 'disabled');
+}
+
+function onClickStopBtn() {
+  clearInterval(colorIntervalId);
+
+  refs.startBtnEl.removeAttribute('disabled');
+  refs.stopBtnEl.setAttribute('disabled', 'disabled');
+}
+
+function generateColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
